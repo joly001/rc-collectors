9cff8864830b4d63d794fc1c5c47ee4cf06c3482
joly001 <joly_huang@163.com>
2019-04-29 17:54:38 +0800

    init

---
+++ src/main/java/com/zcsoft/rc/collectors/app/components/LocationComponent.java
@@ -0,0 +1,82 @@
+package com.zcsoft.rc.collectors.app.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocationComponent {
+
+    public static double pi = 3.1415926535897932384626;
+    public static double a = 6378245.0;
+    public static double ee = 0.00669342162296594323;
+
+    public static double transformLat(double x, double y) {
+        double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y
+                + 0.2 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    public static double transformLon(double x, double y) {
+        double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1
+                * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0
+                * pi)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    public Coordinate gcj02To84(double lon, double lat) {
+        double dLat = transformLat(lon - 105.0, lat - 35.0);
+        double dLon = transformLon(lon - 105.0, lat - 35.0);
+        double radLat = lat / 180.0 * pi;
+        double magic = Math.sin(radLat);
+        magic = 1 - ee * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+        dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi);
+        double mgLat = lat + dLat;
+        double mgLon = lon + dLon;
+
+        double lontitude = lon * 2 - mgLon;
+        double latitude = lat * 2 - mgLat;
+
+        return new Coordinate(lontitude, latitude);
+    }
+
+    public class Coordinate{
+        /**
+         * 经度
+         */
+        private Double longitude;
+        /**
+         * 纬度
+         */
+        private Double latitude;
+
+        public Coordinate(Double longitude, Double latitude) {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+
+        public Double getLongitude() {
+            return longitude;
+        }
+
+        public Double getLatitude() {
+            return latitude;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Coordinate{");
+            sb.append("longitude=").append(longitude);
+            sb.append(", latitude=").append(latitude);
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
+}
--- src/main/java/com/zcsoft/rc/collectors/zc/service/impl/ZcServiceImpl.java
+++ src/main/java/com/zcsoft/rc/collectors/zc/service/impl/ZcServiceImpl.java
@@ -1,6 +1,7 @@
 package com.zcsoft.rc.collectors.zc.service.impl;
 
 import com.zcsoft.rc.collectors.api.zc.entity.ZcReq;
+import com.zcsoft.rc.collectors.app.components.LocationComponent;
 import com.zcsoft.rc.collectors.app.components.websocket.Rc;
 import com.zcsoft.rc.collectors.rc.service.RcService;
 import com.zcsoft.rc.collectors.zc.service.ZcService;
@@ -14,14 +15,23 @@ import javax.annotation.Resource;
 public class ZcServiceImpl implements ZcService {
 
     private RcService rcService;
+    private LocationComponent locationComponent;
 
     @Resource
     public void setRcService(RcService rcService) {
         this.rcService = rcService;
     }
+    @Resource
+    public void setLocationComponent(LocationComponent locationComponent) {
+        this.locationComponent = locationComponent;
+    }
 
     @Override
     public void collect(ZcReq req) {
+        LocationComponent.Coordinate coordinate = locationComponent.gcj02To84(req.getLongitude(), req.getLatitude());
+        req.setLongitude(coordinate.getLongitude());
+        req.setLatitude(coordinate.getLatitude());
+
         Rc rc = new Rc();
         BeanUtils.copyProperties(req, rc);
 
